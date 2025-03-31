974154e9ef60c4546c9d0c196ea9390ec12db34f
Kim Minsu <118744078+yoyobar@users.noreply.github.com>
2025-03-31 22:27:04 +0900

    fix: 주석 수정

---
+++ src/Clock.jsx
@@ -0,0 +1,15 @@
+/**
+ * Clock 컴포넌트
+ *
+ * 실시간 시계를 표시하고 사용자가 시계를 시작하거나 정지할 수 있는 React 함수형 컴포넌트입니다.
+ * 시간은 "시", "분", "초"로 나뉘어 표시됩니다.
+ *
+ * 주요 기능:
+ * - 현재 시간을 "HH:mm:ss" 형식으로 표시합니다.
+ * - 시계가 실행 중일 때 매초마다 시간을 업데이트합니다.
+ **/
+function Clock() {
+  return <div className="timer-container"></div>;
+}
+
+export default Clock;
--- src/Timer.jsx
+++
@@ -1,5 +0,0 @@
-function Timer() {
-  return <div className="timer-container"></div>;
-}
-
-export default Timer;
